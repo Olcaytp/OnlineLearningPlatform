a1d27e4578896ec3e60baee918f83b067a34666f
Olcaytp <olcaytapan10@gmail.com>
2024-05-27 21:19:41 +0200

    new-commit

--- OnlinePlatform/urls.py
+++ OnlinePlatform/urls.py
@@ -1,19 +1,3 @@
-"""
-URL configuration for OnlinePlatform project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.urls import path, include
 from django.contrib import admin
 from django.conf import settings
@@ -23,13 +7,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', user_views.home, name='home'),
-   
-    path('user_register/', user_views.user_register, name='user_register'),
-    path('user_login/', user_views.user_login, name='user_login'),
-    path('logout/', user_views.user_logout, name='user_logout'),
+    path('user/', include('UserApp.urls')),  # Include UserApp URLs
     path('courses/', include('CourseApp.urls')),  # Include CourseApp URLs
-    #path('course_register/', user_views.user_register, name='user_register'),
-
 ]
 
 # Add this line to serve media files during development
